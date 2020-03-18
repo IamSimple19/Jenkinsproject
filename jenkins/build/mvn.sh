587c7705df5991a2451eb583e2e2c3b157d6b649
IamSimple19 <61102406+IamSimple19@users.noreply.github.com>
2020-03-18 18:00:41 +0530

    updateda

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -3,6 +3,7 @@
 echo "**************************************"
 echo "*********Pulling the mvn image*********"
 echo "***************************************"
+docker pull maven:3-alpine
 
 
 docker container run -it -v $PWD/javaproject:/app maven:3-alpine "$@" 
