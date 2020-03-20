08389dc09982a958e4476787a77d82927e51be0d
IamSimple19 <61102406+IamSimple19@users.noreply.github.com>
2020-03-20 14:08:19 +0530

    udated

--- jenkins/build/mvn.sh
+++ jenkins/build/mvn.sh
@@ -6,7 +6,7 @@ echo "***************************************"
 docker pull maven:3-alpine
 
 
-docker container run -it -v $PWD/javaproject:/app maven:3-alpine "$@" 
+docker container run  -v $PWD/javaproject:/app maven:3-alpine  mvn -B -DskipTests clean package && mvn test
 
 
 
